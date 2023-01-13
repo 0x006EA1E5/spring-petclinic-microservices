16d1af9a5e3909f3457a521b625b567a41b6ae45
Greg Eales <gregory.eales@ocado.com>
2023-01-13 17:48:13 +0000

    wip

--- spring-petclinic-common/src/main/java/org/springframework/samples/petclinic/common/OtelConfiguration.java
+++ spring-petclinic-common/src/main/java/org/springframework/samples/petclinic/common/OtelConfiguration.java
@@ -3,13 +3,17 @@ package org.springframework.samples.petclinic.common;
 import io.micrometer.core.instrument.Clock;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.opentelemetry.api.trace.Span;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exemplars.DefaultExemplarSampler;
 import io.prometheus.client.exemplars.ExemplarSampler;
 import io.prometheus.client.exemplars.tracer.common.SpanContextSupplier;
 import io.prometheus.client.exemplars.tracer.otel.OpenTelemetrySpanContextSupplier;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 @Configuration(proxyBeanMethods = false)
 class OtelConfiguration {
@@ -26,5 +30,15 @@ class OtelConfiguration {
     @Bean
     SpanContextSupplier spanContextSupplier() {
         return new OpenTelemetrySpanContextSupplier();
+    }
+
+    HandlerInterceptor spanHandlerInterceptor() {
+        return new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+                Span.current().setAttribute("sampling.priority", 10);
+                return true;
+            }
+        };
     }
 }
