1b00263b1e51db88dace4a1d610a65ee964651d1
Greg Eales <gregory.eales@ocado.com>
2023-01-13 12:12:42 +0000

    otel config

---
+++ spring-petclinic-common/src/main/java/org/springframework/samples/petclinic/common/OtelConfiguration.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.common;
+
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exemplars.DefaultExemplarSampler;
+import io.prometheus.client.exemplars.ExemplarSampler;
+import io.prometheus.client.exemplars.tracer.common.SpanContextSupplier;
+import io.prometheus.client.exemplars.tracer.otel.OpenTelemetrySpanContextSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+class OtelConfiguration {
+    @Bean
+    PrometheusMeterRegistry prometheusMeterRegistry(PrometheusConfig prometheusConfig, CollectorRegistry collectorRegistry, Clock clock, ExemplarSampler exemplarSampler) {
+        return new PrometheusMeterRegistry(prometheusConfig, collectorRegistry, clock, exemplarSampler);
+    }
+
+    @Bean
+    ExemplarSampler exemplarSampler(SpanContextSupplier spanContextSupplier) {
+        return new DefaultExemplarSampler(spanContextSupplier);
+    }
+
+    @Bean
+    SpanContextSupplier spanContextSupplier() {
+        return new OpenTelemetrySpanContextSupplier();
+    }
+}
