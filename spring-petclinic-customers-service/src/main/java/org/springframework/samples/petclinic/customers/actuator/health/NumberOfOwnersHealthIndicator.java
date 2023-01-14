9e4f40377ad9e0c3288635766fa09dc6a1e05ae9
Stefano Boriero <stefano.boriero@ocado.com>
2023-01-14 01:55:41 +0100

    Use JarJar compliant HealthDetailsIndicator

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/actuator/health/NumberOfOwnersHealthIndicator.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/actuator/health/NumberOfOwnersHealthIndicator.java
@@ -1,13 +1,17 @@
 package org.springframework.samples.petclinic.customers.actuator.health;
 
+import com.ocadotechnology.jarjar.core.health.model.HealthDetails;
+import com.ocadotechnology.jarjar.core.health.model.HealthStatus;
+import com.ocadotechnology.jarjar.core.health.service.HealthDetailsIndicator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.samples.petclinic.customers.model.OwnerRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NumberOfOwnersHealthIndicator implements HealthIndicator {
+public class NumberOfOwnersHealthIndicator implements HealthIndicator, HealthDetailsIndicator {
     private final OwnerRepository ownerRepository;
 
     @Autowired
@@ -17,9 +21,19 @@ public class NumberOfOwnersHealthIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
-        if (ownerRepository.findAll().size() > 15) {
-            return Health.down().build();
-        }
-        return Health.up().build();
+        return ownerRepository.findAll().size() > 15
+            ? Health.down().build()
+            : Health.up().build();
+    }
+
+    @Override
+    public HealthDetails getHealthDetails() {
+        var healthStatus = health().getStatus().equals(Status.DOWN)
+            ? HealthStatus.unhealthy
+            : HealthStatus.healthy;
+        return HealthDetails.Builder
+            .healthDetails()
+            .withHealth(healthStatus)
+            .build();
     }
 }
