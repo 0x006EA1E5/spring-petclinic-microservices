de15fdd5e0ed13bcc4a154ba58d0e07802dea8d8
Greg Eales <gregory.eales@ocado.com>
2023-01-14 16:59:36 +0000

    wip

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/Visits.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/Visits.java
@@ -1,6 +1,14 @@
 package org.springframework.samples.petclinic.visits.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
-public record Visits(List<Visit> items) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visits {
+    private List<Visit> items;
 }
