477c0d1e549d4e3b320f0c0fa55ed6ab88c7cc14
Greg Eales <gregory.eales@ocado.com>
2023-01-25 16:01:17 +0000

    fix with @Primary

--- spring-petclinic-common/src/main/java/org/springframework/samples/petclinic/common/OtelConfiguration.java
+++ spring-petclinic-common/src/main/java/org/springframework/samples/petclinic/common/OtelConfiguration.java
@@ -3,12 +3,20 @@ package org.springframework.samples.petclinic.common;
 import io.prometheus.client.exemplars.DefaultExemplarSampler;
 import io.prometheus.client.exemplars.ExemplarSampler;
 import io.prometheus.client.exemplars.tracer.common.SpanContextSupplier;
+import io.prometheus.client.exemplars.tracer.otel.OpenTelemetrySpanContextSupplier;
 import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration(proxyBeanMethods = false)
+@AutoConfigureBefore(PrometheusMetricsExportAutoConfiguration.class)
 class OtelConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(OtelConfiguration.class);
 //    @Bean
 //    PrometheusMeterRegistry prometheusMeterRegistry(PrometheusConfig prometheusConfig, CollectorRegistry collectorRegistry, Clock clock, ExemplarSampler exemplarSampler) {
 //        return new PrometheusMeterRegistry(prometheusConfig, collectorRegistry, clock, exemplarSampler);
@@ -21,11 +29,22 @@ class OtelConfiguration {
 
     @Bean
     SpanContextSupplier spanContextSupplier() {
-        return new OpenTelemetryAgentSpanContextSupplier();
+        if (OpenTelemetryAgentSpanContextSupplier.isAvailable()) {
+            logger.debug("OpenTelemetryAgentSpanContextSupplier is available, using it.");
+            return new OpenTelemetryAgentSpanContextSupplier();
+        }
+        return new OpenTelemetrySpanContextSupplier();
     }
 
     @Bean
+    @Primary
     ExemplarSampler exemplarMarkingExemplarSampler(SpanContextSupplier spanContextSupplier) {
-        return new ExemplarMarkingExemplarSampler(new DefaultExemplarSampler(spanContextSupplier));
+        var defaultExemplarSampler = new DefaultExemplarSampler(spanContextSupplier);
+        if (OpenTelemetryAgentSpanContextSupplier.isAvailable()) {
+            logger.debug("OpenTelemetryAgentSpanContextSupplier is available, using ExemplarMarkingExemplarSampler");
+            return new ExemplarMarkingExemplarSampler(defaultExemplarSampler);
+        }
+        return defaultExemplarSampler;
+
     }
 }
